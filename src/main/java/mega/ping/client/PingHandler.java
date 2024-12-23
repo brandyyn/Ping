fd634c292464c3a17da844899a76fdb1235f997d
FalsePattern <me@falsepattern.com>
2024-12-23 21:37:43 +0100

    Revert "add beacon effect to goto pings"
    This reverts commit 4272af8f2d18c2296e5fc5d2c19ba94cc7087409.

--- src/main/java/mega/ping/client/PingHandler.java
+++ src/main/java/mega/ping/client/PingHandler.java
@@ -40,9 +40,7 @@ import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.IIcon;
-import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -64,7 +62,6 @@ public class PingHandler {
     public static final PingHandler INSTANCE = new PingHandler();
 
     public static final ResourceLocation TEXTURE = new ResourceLocation("ping:textures/ping.png");
-    public static final ResourceLocation BEAM_TEXTURE = new ResourceLocation("textures/entity/beacon_beam.png");
 
     private static final ResourceLocation APPEAR_SOUND = new ResourceLocation("ping:appear");
     private static final ResourceLocation DISAPPEAR_SOUND = new ResourceLocation("ping:disappear");
@@ -107,13 +104,8 @@ public class PingHandler {
 
             if (camera.isBoundingBoxInFrustum(ping.getAABB())) {
                 ping.isOffscreen = false;
-
                 if (PingConfig.Visual.blockOverlay) {
-                    this.renderPingOverlay(ping.x - TileEntityRendererDispatcher.staticPlayerX,
-                                           ping.y - TileEntityRendererDispatcher.staticPlayerY,
-                                           ping.z - TileEntityRendererDispatcher.staticPlayerZ,
-                                           ping,
-                                           event.partialTicks);
+                    renderPingOverlay(ping.x - TileEntityRendererDispatcher.staticPlayerX, ping.y - TileEntityRendererDispatcher.staticPlayerY, ping.z - TileEntityRendererDispatcher.staticPlayerZ, ping);
                 }
                 renderPing(px, py, pz, renderEntity, ping);
             } else {
@@ -277,7 +269,7 @@ public class PingHandler {
         GL11.glPopMatrix();
     }
 
-    public void renderPingOverlay(double x, double y, double z, PingWrapper ping, float partialTicks) {
+    public void renderPingOverlay(double x, double y, double z, PingWrapper ping) {
         Minecraft mc = Minecraft.getMinecraft();
         if (renderBlocks == null || renderBlocks.blockAccess != mc.theWorld) {
             renderBlocks = new RenderBlocks(mc.theWorld);
@@ -297,10 +289,6 @@ public class PingHandler {
         GL11.glDisable(GL11.GL_DEPTH_TEST);
 
         Tessellator.instance.setTranslation(x + 0.5, y + 0.5, z + 0.5);
-
-        if (ping.type == PingType.GOTO) {
-            this.renderBeamShaft(mc.theWorld, x, y, z, partialTicks);
-        }
 
         PingRenderHelper.drawBlockOverlay(box, box, box, icon, ping.color, 150 + alpha);
 
@@ -328,104 +316,5 @@ public class PingHandler {
             ping.screenY = screenCoords.get(1);
             ping.behind = screenCoords.get(2) > 1;
         }
-    }
-
-    private void renderBeamShaft(World world, double x, double y, double z, float partialTicks) {
-        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-        GL11.glPushMatrix();
-
-        GL11.glAlphaFunc(GL11.GL_GREATER, 0.1F);
-
-        Tessellator tessellator = Tessellator.instance;
-        Minecraft.getMinecraft().renderEngine.bindTexture(BEAM_TEXTURE);
-
-        GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, 10497.0F);
-        GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, 10497.0F);
-        GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_CULL_FACE);
-        GL11.glDisable(GL11.GL_BLEND);
-        GL11.glDepthMask(true);
-        OpenGlHelper.glBlendFunc(770, 1, 1, 0);
-
-        float beamHeight = 1F;
-
-        float time = (float) world.getTotalWorldTime() + partialTicks;
-        float f3 = -time * 0.2F - (float) MathHelper.floor_float(-time * 0.1F);
-
-        double d3 = (double) time * 0.025D * (1.0D - 2.5D);
-        tessellator.startDrawingQuads();
-        tessellator.setColorRGBA(255, 255, 255, 32);
-        double d5 = 0.2D;
-        double d7 = 0.5D + Math.cos(d3 + 2.356194490192345D) * d5;
-        double d9 = 0.5D + Math.sin(d3 + 2.356194490192345D) * d5;
-        double d11 = 0.5D + Math.cos(d3 + (Math.PI / 4D)) * d5;
-        double d13 = 0.5D + Math.sin(d3 + (Math.PI / 4D)) * d5;
-        double d15 = 0.5D + Math.cos(d3 + 3.9269908169872414D) * d5;
-        double d17 = 0.5D + Math.sin(d3 + 3.9269908169872414D) * d5;
-        double d19 = 0.5D + Math.cos(d3 + 5.497787143782138D) * d5;
-        double d21 = 0.5D + Math.sin(d3 + 5.497787143782138D) * d5;
-        double d23 = 256.0F * beamHeight;
-        double d25 = 0.0D;
-        double d27 = 1.0D;
-        double d28 = -1.0F + f3;
-        double d29 = (double)(256.0F * beamHeight) * (0.5D / d5) + d28;
-        tessellator.addVertexWithUV(x + d7, y + d23, z + d9, d27, d29);
-        tessellator.addVertexWithUV(x + d7, y, z + d9, d27, d28);
-        tessellator.addVertexWithUV(x + d11, y, z + d13, d25, d28);
-        tessellator.addVertexWithUV(x + d11, y + d23, z + d13, d25, d29);
-        tessellator.addVertexWithUV(x + d19, y + d23, z + d21, d27, d29);
-        tessellator.addVertexWithUV(x + d19, y, z + d21, d27, d28);
-        tessellator.addVertexWithUV(x + d15, y, z + d17, d25, d28);
-        tessellator.addVertexWithUV(x + d15, y + d23, z + d17, d25, d29);
-        tessellator.addVertexWithUV(x + d11, y + d23, z + d13, d27, d29);
-        tessellator.addVertexWithUV(x + d11, y, z + d13, d27, d28);
-        tessellator.addVertexWithUV(x + d19, y, z + d21, d25, d28);
-        tessellator.addVertexWithUV(x + d19, y + d23, z + d21, d25, d29);
-        tessellator.addVertexWithUV(x + d15, y + d23, z + d17, d27, d29);
-        tessellator.addVertexWithUV(x + d15, y, z + d17, d27, d28);
-        tessellator.addVertexWithUV(x + d7, y, z + d9, d25, d28);
-        tessellator.addVertexWithUV(x + d7, y + d23, z + d9, d25, d29);
-        tessellator.draw();
-        GL11.glEnable(GL11.GL_BLEND);
-        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
-        GL11.glDepthMask(false);
-        tessellator.startDrawingQuads();
-        tessellator.setColorRGBA(255, 255, 255, 32);
-        double d30 = 0.2D;
-        double d4 = 0.2D;
-        double d6 = 0.8D;
-        double d8 = 0.2D;
-        double d10 = 0.2D;
-        double d12 = 0.8D;
-        double d14 = 0.8D;
-        double d16 = 0.8D;
-        double d18 = 256.0F * beamHeight;
-        double d20 = 0.0D;
-        double d22 = 1.0D;
-        double d24 = -1.0F + f3;
-        double d26 = (double) (256.0F * beamHeight) + d24;
-        tessellator.addVertexWithUV(x + d30, y + d18, z + d4, d22, d26);
-        tessellator.addVertexWithUV(x + d30, y, z + d4, d22, d24);
-        tessellator.addVertexWithUV(x + d6, y, z + d8, d20, d24);
-        tessellator.addVertexWithUV(x + d6, y + d18, z + d8, d20, d26);
-        tessellator.addVertexWithUV(x + d14, y + d18, z + d16, d22, d26);
-        tessellator.addVertexWithUV(x + d14, y, z + d16, d22, d24);
-        tessellator.addVertexWithUV(x + d10, y, z + d12, d20, d24);
-        tessellator.addVertexWithUV(x + d10, y + d18, z + d12, d20, d26);
-        tessellator.addVertexWithUV(x + d6, y + d18, z + d8, d22, d26);
-        tessellator.addVertexWithUV(x + d6, y, z + d8, d22, d24);
-        tessellator.addVertexWithUV(x + d14, y, z + d16, d20, d24);
-        tessellator.addVertexWithUV(x + d14, y + d18, z + d16, d20, d26);
-        tessellator.addVertexWithUV(x + d10, y + d18, z + d12, d22, d26);
-        tessellator.addVertexWithUV(x + d10, y, z + d12, d22, d24);
-        tessellator.addVertexWithUV(x + d30, y, z + d4, d20, d24);
-        tessellator.addVertexWithUV(x + d30, y + d18, z + d4, d20, d26);
-        tessellator.draw();
-        GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glEnable(GL11.GL_TEXTURE_2D);
-        GL11.glDepthMask(true);
-
-        GL11.glPopMatrix();
-        GL11.glPopAttrib();
     }
 }
